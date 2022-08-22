1fe44aa9293a7296772bb6e8645e3c10a19083fe
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2022-08-22 18:23:44 +0200

    Fixed routing to react routes besides the /api/** route (thanks to: https://stackoverflow.com/users/352308/chaholl, thread-ref: https://stackoverflow.com/questions/47689971/how-to-work-with-react-routers-and-spring-boot-controller )

---
+++ java/web/src/main/java/be/patryksitko/contest/ip2location/com/controller/ReactAppController.java
@@ -0,0 +1,16 @@
+package be.patryksitko.contest.ip2location.com.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ReactAppController {
+
+    @RequestMapping(value = { "/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/*/{y:[\\w\\-]+}", "/error" })
+    public String getIndex(HttpServletRequest request) {
+        return "/index.html";
+    }
+
+}
