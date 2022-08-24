50c5a42b0349549266a215564cad97c6df1f2014
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2022-08-24 11:36:49 +0200

    Created an user repository.

---
+++ java/web/src/main/java/be/patryksitko/contest/ip2location/com/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package be.patryksitko.contest.ip2location.com.repository;
+
+import org.springframework.data.repository.Repository;
+
+import be.patryksitko.contest.ip2location.com.model.User;
+
+public interface UserRepository extends Repository<User, Long> {
+
+}
