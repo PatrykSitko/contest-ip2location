de2e83be6fd0bc0fb594b212926d8c29fa3f54cc
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2022-08-24 12:29:46 +0200

    Created an user service.

---
+++ java/web/src/main/java/be/patryksitko/contest/ip2location/com/service/UserService.java
@@ -0,0 +1,8 @@
+package be.patryksitko.contest.ip2location.com.service;
+
+import be.patryksitko.contest.ip2location.com.model.User;
+
+public interface UserService {
+
+    public void createUser(User user);
+}
