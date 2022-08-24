e9ef5df3c04cc44af6d4a90f2e336cc02514ff1a
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2022-08-24 02:34:31 +0200

    Working on user model.

---
+++ java/web/src/main/java/be/patryksitko/contest/ip2location/com/model/User.java
@@ -0,0 +1,74 @@
+package be.patryksitko.contest.ip2location.com.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.fasterxml.jackson.annotation.JacksonInject;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.ToString;
+
+@Builder
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@JsonRootName(value = "user", namespace = "users")
+@JsonPropertyOrder({ "id", "firstname", "lastname", "email" })
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @NonNull
+    @GeneratedValue
+    @JacksonInject
+    private Long id;
+
+    @JsonProperty("firstname")
+    @Column(name = "firstname", nullable = false)
+    public String firstname;
+
+    @JsonProperty("lastname")
+    @Column(name = "lastname", nullable = false)
+    public String lastname;
+
+    @JsonProperty("email")
+    @Column(name = "email", nullable = false, unique = true)
+    public String email;
+
+    @Getter(onMethod = @__(@JsonIgnore))
+    @Setter(onMethod = @__(@JsonProperty("password")))
+    @JsonAlias({ "password", "passwd" })
+    @Column(name = "password", nullable = false)
+    public String password;
+
+    @JsonCreator
+    public User(@JsonProperty("firstname") String firstname, @JsonProperty("lastname") String lastname,
+            @JsonProperty("email") String email, @JsonProperty("password") String password) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = new BCryptPasswordEncoder().encode(password);
+    }
+}
