3502d2d3bf1dabcc7e05635ea7d7550271788ff4
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2022-08-25 00:27:48 +0200

    Declared throws fo createUser method.

--- java/web/src/main/java/be/patryksitko/contest/ip2location/com/service/UserService.java
+++ java/web/src/main/java/be/patryksitko/contest/ip2location/com/service/UserService.java
@@ -1,8 +1,9 @@
 package be.patryksitko.contest.ip2location.com.service;
 
 import be.patryksitko.contest.ip2location.com.model.User;
+import be.patryksitko.contest.ip2location.com.service.exception.UserExistsException;
 
 public interface UserService {
 
-    public void createUser(User user);
+    public User createUser(User user) throws UserExistsException;
 }
