759084839b66fb58e183492ad8fddbb1bf3b592f
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2022-08-25 00:27:26 +0200

    Removed user created exception.

--- java/web/src/main/java/be/patryksitko/contest/ip2location/com/service/exception/UserCreatedException.java
+++
@@ -1,8 +0,0 @@
-package be.patryksitko.contest.ip2location.com.service.exception;
-
-public class UserCreatedException extends RuntimeException {
-
-    public UserCreatedException(String email) {
-        super("Registered new user under \"" + email + " email.");
-    }
-}
