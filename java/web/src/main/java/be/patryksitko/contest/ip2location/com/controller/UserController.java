dc442e4c4d1f43af98c08506ddf5c379a1e91e90
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2022-08-25 00:28:28 +0200

    Created a post mapping for create user service method.

---
+++ java/web/src/main/java/be/patryksitko/contest/ip2location/com/controller/UserController.java
@@ -0,0 +1,27 @@
+package be.patryksitko.contest.ip2location.com.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import be.patryksitko.contest.ip2location.com.model.User;
+import be.patryksitko.contest.ip2location.com.service.UserService;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping(value = "/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody User createUser(@RequestBody @Valid User user) {
+        return userService.createUser(user);
+    }
+}
