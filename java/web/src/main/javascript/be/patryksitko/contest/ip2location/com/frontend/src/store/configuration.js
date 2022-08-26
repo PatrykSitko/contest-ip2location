17acb63297b68acba58e14480979188856822e0a
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2022-08-26 10:38:32 +0200

    Created a copy of the routerMiddleware from redux-first-routing and implemented it as the use of the hook: useNavigate while wanting to maintain redux functionality. Created duplicate entries what messes with goBack and other reducers.

--- java/web/src/main/javascript/be/patryksitko/contest/ip2location/com/frontend/src/store/configuration.js
+++ java/web/src/main/javascript/be/patryksitko/contest/ip2location/com/frontend/src/store/configuration.js
@@ -1,6 +1,7 @@
 import { applyMiddleware } from "redux";
-import { createBrowserHistory, routerMiddleware } from "redux-first-routing";
+import { createBrowserHistory } from "redux-first-routing";
 import thunk from "redux-thunk";
+import routerMiddleware from "./middleware/redux-first-routing/routerMiddleware";
 import reducers from "./reducers";
 
 export const browserHistory = (() => createBrowserHistory())();
