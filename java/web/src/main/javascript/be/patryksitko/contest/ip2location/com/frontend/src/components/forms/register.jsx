624518906c41c953ea3fc6c0f48d6bd16adc901f
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2022-08-29 15:07:32 +0200

    Form validation finnished.

--- java/web/src/main/javascript/be/patryksitko/contest/ip2location/com/frontend/src/components/forms/register.jsx
+++ java/web/src/main/javascript/be/patryksitko/contest/ip2location/com/frontend/src/components/forms/register.jsx
@@ -1,143 +1,165 @@
-import { Formik } from "formik";
+import { useFormik } from "formik";
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { connect } from "react-redux";
 import { goBack } from "redux-first-routing";
-import * as yup from "yup";
+import * as Yup from "yup";
 import "./register.scss";
 
-const schema = yup.object({
-  firstname: yup
-    .string()
+const validationSchema = Yup.object({
+  firstname: Yup.string()
     .matches(/^[a-zA-Z]+$/, "Must only contain letters.")
     .required("Firstname is required."),
-  lastname: yup
-    .string()
+  lastname: Yup.string()
     .matches(/^[a-zA-Z]+$/, "Must only contain letters.")
     .required("Lastname is required."),
-  email: yup.string().email().required("Email is required."),
-  password: yup.string().strongPassword().required("Password is required."),
-  repassword: yup.string().required("Retyping password is required."),
+  email: Yup.string().email().required("Email is required."),
+  password: Yup.string().strongPassword().required("Password is required."),
+  repassword: Yup.string()
+    .required("Retyping password is required.")
+    .oneOf([Yup.ref("password"), null], "Passwords don't match!"),
 });
+const initialValues = {
+  firstname: "",
+  lastname: "",
+  email: "",
+  password: "",
+  repassword: "",
+};
 
 const mapDispatchToProps = (dispatch) => ({
   goBack: (path) => dispatch(goBack(path)),
 });
 
 function LoginForm({ goBack }) {
+  const formik = useFormik({
+    validationSchema,
+    initialValues,
+    onSubmit: (values, { setSubmitting }) => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    },
+  });
   return (
     <div className="register-form-wrapper">
-      <Formik
-        validationSchema={schema}
-        initialValues={{ firstname: "", lastname: "", email: "", password: "" }}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-        }) => (
-          <Form className="register-form" onSubmit={handleSubmit}>
-            <Form.Group
-              className="mb-3 given-names-wrapper firstname"
-              controlId="formFirstname"
-            >
-              <Form.Label className="given-names">First name</Form.Label>
-              <Form.Control
-                name="firstname"
-                className="given-names"
-                type="text"
-                placeholder="Enter first name"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.firstname}
-                isValid={!errors.firstname && values.firstname !== ""}
-                isInvalid={touched.firstname && errors.firstname}
-                feedback={errors.firstname}
-              />
-              <Form.Control.Feedback type="invalid">
-                {errors.firstname}
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group
-              className="mb-3 given-names-wrapper lastname"
-              controlId="formLastname"
-            >
-              <Form.Label className="given-names">Last name</Form.Label>
-              <Form.Control
-                name="lastname"
-                className="given-names"
-                type="text"
-                placeholder="Enter last name"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.lastname}
-                isValid={!errors.lastname && values.lastname !== ""}
-                isInvalid={touched.lastname && errors.lastname}
-                feedback={errors.lastname}
-              />
-              <Form.Control.Feedback type="invalid">
-                {errors.lastname}
-              </Form.Control.Feedback>
-            </Form.Group>
+      <Form className="register-form" onSubmit={formik.handleSubmit}>
+        <Form.Group
+          className="mb-3 given-names-wrapper firstname"
+          controlId="formFirstname"
+        >
+          <Form.Label className="given-names">First name</Form.Label>
+          <Form.Control
+            disabled={formik.isSubmitting}
+            name="firstname"
+            className="given-names"
+            type="text"
+            placeholder="Enter first name"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.firstname}
+            isValid={!formik.errors.firstname && formik.values.firstname !== ""}
+            isInvalid={formik.touched.firstname && formik.errors.firstname}
+            feedback={formik.errors.firstname}
+          />
+          <Form.Control.Feedback type="invalid">
+            {formik.errors.firstname}
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group
+          className="mb-3 given-names-wrapper lastname"
+          controlId="formLastname"
+        >
+          <Form.Label className="given-names">Last name</Form.Label>
+          <Form.Control
+            disabled={formik.isSubmitting}
+            name="lastname"
+            className="given-names"
+            type="text"
+            placeholder="Enter last name"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.lastname}
+            isValid={!formik.errors.lastname && formik.values.lastname !== ""}
+            isInvalid={formik.touched.lastname && formik.errors.lastname}
+            feedback={formik.errors.lastname}
+          />
+          <Form.Control.Feedback type="invalid">
+            {formik.errors.lastname}
+          </Form.Control.Feedback>
+        </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formEmail">
-              <Form.Label className="email">Email address</Form.Label>
-              <Form.Control
-                name="email"
-                type="email"
-                placeholder="Enter email"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.email}
-                isValid={!errors.email && values.email !== ""}
-                isInvalid={touched.email && errors.email}
-                feedback={errors.email}
-              />
-              <Form.Control.Feedback type="invalid">
-                {errors.email}
-              </Form.Control.Feedback>
-            </Form.Group>
+        <Form.Group className="mb-3" controlId="formEmail">
+          <Form.Label className="email">Email address</Form.Label>
+          <Form.Control
+            disabled={formik.isSubmitting}
+            name="email"
+            type="email"
+            placeholder="Enter email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+            isValid={!formik.errors.email && formik.values.email !== ""}
+            isInvalid={formik.touched.email && formik.errors.email}
+            feedback={formik.errors.email}
+          />
+          <Form.Control.Feedback type="invalid">
+            {formik.errors.email}
+          </Form.Control.Feedback>
+        </Form.Group>
 
-            <Form.Group
-              className="mb-3 form-group-password"
-              controlId="formPassword"
-            >
-              <Button className="go-back" onClick={goBack}>
-                go back
-              </Button>
-              <Form.Label className="password-label">Password</Form.Label>
-              <Form.Control
-                name="password"
-                className="type-password"
-                type="password"
-                placeholder="Password"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.password}
-                isValid={!errors.password && values.password !== ""}
-                isInvalid={touched.password && errors.password}
-                feedback={errors.password}
-              />
-            </Form.Group>
-            <div className="feedback-invalid">{errors.password}</div>
+        <Form.Group
+          className="mb-3 form-group-password"
+          controlId="formPassword"
+        >
+          <Button className="go-back" onClick={goBack}>
+            go back
+          </Button>
+          <Form.Label className="password-label">Password</Form.Label>
+          <Form.Control
+            disabled={formik.isSubmitting}
+            name="password"
+            type="password"
+            placeholder="Password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+            isValid={!formik.errors.password && formik.values.password !== ""}
+            isInvalid={formik.touched.password && formik.errors.password}
+            feedback={formik.errors.password}
+          />
+        </Form.Group>
+        <div visible={formik.errors.password} className="feedback-invalid">
+          {formik.errors.password}
+        </div>
 
-            <Form.Group className="mb-3" controlId="formRetypePassword">
-              <Form.Label className="password-label">
-                Retype Password
-              </Form.Label>
-              <Form.Control type="password" placeholder="Retype Password" />
-            </Form.Group>
-            <Button variant="primary" type="submit" className="register">
-              Register
-            </Button>
-          </Form>
-        )}
-      </Formik>
+        <Form.Group className="mb-3" controlId="formRetypePassword">
+          <Form.Label className="password-label">Retype Password</Form.Label>
+          <Form.Control
+            disabled={formik.isSubmitting}
+            name="repassword"
+            type="password"
+            placeholder="Retype Password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.repassword}
+            isValid={
+              !formik.errors.repassword && formik.values.repassword !== ""
+            }
+            isInvalid={formik.touched.repassword && formik.errors.repassword}
+            feedback={formik.errors.repassword}
+          />
+        </Form.Group>
+        <div className="feedback-invalid">{formik.errors.repassword}</div>
+        <Button
+          variant="primary"
+          type="submit"
+          className="register"
+          disabled={!(formik.isValid && formik.dirty) || formik.isSubmitting}
+        >
+          Register
+        </Button>
+      </Form>
     </div>
   );
 }
