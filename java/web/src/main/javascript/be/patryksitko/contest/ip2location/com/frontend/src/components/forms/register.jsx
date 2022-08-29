383f1723b7479188067be11cbc6a7189464c5ea4
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2022-08-29 13:59:02 +0200

    Working on register form.

--- java/web/src/main/javascript/be/patryksitko/contest/ip2location/com/frontend/src/components/forms/register.jsx
+++ java/web/src/main/javascript/be/patryksitko/contest/ip2location/com/frontend/src/components/forms/register.jsx
@@ -1,69 +1,147 @@
+import { Formik } from "formik";
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { connect } from "react-redux";
 import { goBack } from "redux-first-routing";
+import * as yup from "yup";
 import "./register.scss";
+
+const schema = yup.object({
+  firstname: yup
+    .string()
+    .matches(/^[a-zA-Z]+$/, "Must only contain letters.")
+    .required("Firstname is required."),
+  lastname: yup
+    .string()
+    .matches(/^[a-zA-Z]+$/, "Must only contain letters.")
+    .required("Lastname is required."),
+  email: yup.string().email().required("Email is required."),
+  password: yup
+    .string()
+    .min(8, "Password must contain atleast 8 characters.")
+    .strongPassword()
+    .required("Password is required."),
+  repassword: yup.string().required("Retyping password is required."),
+});
 
 const mapDispatchToProps = (dispatch) => ({
   goBack: (path) => dispatch(goBack(path)),
 });
+
 function LoginForm({ goBack }) {
   return (
     <div className="register-form-wrapper">
-      <Button className="go-back" onClick={goBack}>
-        go back
-      </Button>
-      <Form className="register-form">
-        <Form.Group
-          className="mb-3 given-names-wrapper"
-          controlId="formFirstname"
-        >
-          <Form.Label className="given-names">First name</Form.Label>
-          <Form.Control
-            className="given-names"
-            type="text"
-            placeholder="Enter first name"
-          />
-        </Form.Group>
+      <Formik
+        validationSchema={schema}
+        initialValues={{ firstname: "", lastname: "", email: "", password: "" }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <Form className="register-form" onSubmit={handleSubmit}>
+            <Form.Group
+              className="mb-3 given-names-wrapper firstname"
+              controlId="formFirstname"
+            >
+              <Form.Label className="given-names">First name</Form.Label>
+              <Form.Control
+                name="firstname"
+                className="given-names"
+                type="text"
+                placeholder="Enter first name"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.firstname}
+                isValid={!errors.firstname && values.firstname !== ""}
+                isInvalid={touched.firstname && errors.firstname}
+                feedback={errors.firstname}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.firstname}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group
+              className="mb-3 given-names-wrapper lastname"
+              controlId="formLastname"
+            >
+              <Form.Label className="given-names">Last name</Form.Label>
+              <Form.Control
+                name="lastname"
+                className="given-names"
+                type="text"
+                placeholder="Enter last name"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.lastname}
+                isValid={!errors.lastname && values.lastname !== ""}
+                isInvalid={touched.lastname && errors.lastname}
+                feedback={errors.lastname}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.lastname}
+              </Form.Control.Feedback>
+            </Form.Group>
 
-        <Form.Group
-          className="mb-3 given-names-wrapper"
-          controlId="formLastname"
-        >
-          <Form.Label className="given-names">Last name</Form.Label>
-          <Form.Control
-            className="given-names"
-            type="text"
-            placeholder="Enter last name"
-          />
-        </Form.Group>
+            <Form.Group className="mb-3" controlId="formEmail">
+              <Form.Label className="email">Email address</Form.Label>
+              <Form.Control
+                name="email"
+                type="email"
+                placeholder="Enter email"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+                isValid={!errors.email && values.email !== ""}
+                isInvalid={touched.email && errors.email}
+                feedback={errors.email}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.email}
+              </Form.Control.Feedback>
+            </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formEmail">
-          <Form.Label className="email">Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
+            <Form.Group
+              className="mb-3 form-group-password"
+              controlId="formPassword"
+            >
+              <Button className="go-back" onClick={goBack}>
+                go back
+              </Button>
+              <Form.Label className="password-label">Password</Form.Label>
+              <Form.Control
+                name="password"
+                className="type-password"
+                type="password"
+                placeholder="Password"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.password}
+                isValid={!errors.password && values.password !== ""}
+                isInvalid={touched.password && errors.password}
+                feedback={errors.password}
+              />
+            </Form.Group>
+            <div className="feedback-invalid">{errors.password}</div>
 
-        <Form.Group
-          className="mb-3 form-group-password"
-          controlId="formPassword"
-        >
-          <Form.Label className="password-label">Password</Form.Label>
-          <Form.Control
-            className="type-password"
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formRetypePassword">
-          <Form.Label className="password-label">Retype Password</Form.Label>
-          <Form.Control type="password" placeholder="Retype Password" />
-        </Form.Group>
-        <Button variant="primary" type="submit" className="register">
-          Register
-        </Button>
-      </Form>
+            <Form.Group className="mb-3" controlId="formRetypePassword">
+              <Form.Label className="password-label">
+                Retype Password
+              </Form.Label>
+              <Form.Control type="password" placeholder="Retype Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit" className="register">
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
