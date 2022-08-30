11e0b1bf15870671552a5ecd2f070a32bd6163e4
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2022-08-30 14:54:04 +0200

    Implemented route("/api/user/register").

--- java/web/src/main/java/be/patryksitko/contest/ip2location/com/controller/UserController.java
+++ java/web/src/main/java/be/patryksitko/contest/ip2location/com/controller/UserController.java
@@ -1,18 +1,27 @@
 package be.patryksitko.contest.ip2location.com.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import be.patryksitko.contest.ip2location.com.builders.ResponseBuilder;
+import be.patryksitko.contest.ip2location.com.builders.ResponseBuilder.ResponseType;
 import be.patryksitko.contest.ip2location.com.model.User;
 import be.patryksitko.contest.ip2location.com.service.UserService;
+import be.patryksitko.contest.ip2location.com.service.exception.UserExistsException;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
@@ -21,9 +30,20 @@ public class UserController {
     private UserService userService;
 
     @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody User registerUser(@RequestBody @Valid User user) {
-        final User registerdUser = userService.registerUser(user);
-        registerdUser.setPassword("hidden");
-        return registerdUser;
+    public @ResponseBody ResponseEntity<Object> registerUser(@RequestBody @Valid User user) {
+        try {
+            userService.registerUser(user);
+        } catch (UserExistsException uex) {
+            log.error(uex.getMessage());
+            user.setPassword("hidden");
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body(ResponseBuilder.builder().status(HttpStatus.CONFLICT).errors(List.of(uex.getMessage()))
+                            .type(ResponseType.ERROR).body(user.toJSON())
+                            .build());
+        }
+        user.setPassword("hidden");
+        return ResponseEntity.status(HttpStatus.CREATED).body(ResponseBuilder.builder().status(HttpStatus.CREATED)
+                .type(ResponseType.SUCCESS).body(user.toJSON()).build());
+
     }
 }
