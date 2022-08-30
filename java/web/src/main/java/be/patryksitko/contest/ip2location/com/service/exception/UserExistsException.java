f7a3b57d5e54d97d729c594211de715538424c89
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2022-08-30 14:54:38 +0200

    Prepended [email]: inside of the message for easy manipulation inside of the frontend.

--- java/web/src/main/java/be/patryksitko/contest/ip2location/com/service/exception/UserExistsException.java
+++ java/web/src/main/java/be/patryksitko/contest/ip2location/com/service/exception/UserExistsException.java
@@ -3,6 +3,6 @@ package be.patryksitko.contest.ip2location.com.service.exception;
 public class UserExistsException extends RuntimeException {
 
     public UserExistsException(String email) {
-        super("The email \"" + email + "\" is already registered.");
+        super("[EMAIL]:The email \"" + email + "\" is already registered.");
     }
 }
