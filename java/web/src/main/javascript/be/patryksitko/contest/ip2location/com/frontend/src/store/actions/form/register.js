c6f5b192bf75f73c632542445ab3f48830cf4929
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2022-09-01 20:14:19 +0200

    Added login and register actions.

---
+++ java/web/src/main/javascript/be/patryksitko/contest/ip2location/com/frontend/src/store/actions/form/register.js
@@ -0,0 +1,23 @@
+//update tick
+import { STORE_FORM_REGISTER } from "../types";
+
+const registerFormAction = (
+  currentState,
+  register = {
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+    repassword: "",
+  }
+) => ({
+  type: STORE_FORM_REGISTER,
+  payload: {
+    form: {
+      ...currentState.form,
+      register: { ...currentState.form.register, ...register },
+    },
+  },
+});
+
+export default registerFormAction;
--- java/web/src/main/javascript/be/patryksitko/contest/ip2location/com/frontend/src/store/actions/types.js
+++ java/web/src/main/javascript/be/patryksitko/contest/ip2location/com/frontend/src/store/actions/types.js
@@ -5,12 +5,16 @@ export const UPDATE_TICK = "UPDATE_TICK";
 // authentiction
 export const STORE_FINGERPRINT = "STORE_FINGERPRINT";
 export const STORE_COOKIES = "STORE_COOKIES";
+export const STORE_FORM_LOGIN = "STORE_FORM_LOGIN";
+export const STORE_FORM_REGISTER = "STORE_FORM_REGISTER";
 
 const actionTypes = [
   WINDOW_RESIZED,
   UPDATE_TICK,
   STORE_FINGERPRINT,
   STORE_COOKIES,
+  STORE_FORM_LOGIN,
+  STORE_FORM_REGISTER,
 ];
 
 export default actionTypes;
