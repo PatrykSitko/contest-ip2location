d1aaba84a5b9f507b9ddf0dfaf760d5dd6d2953e
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2022-09-01 12:07:50 +0200

    Moved credential saving into UserServiceImplementation as saving an user is dependent upon that action.

--- java/web/src/main/java/be/patryksitko/contest/ip2location/com/controller/UserController.java
+++ java/web/src/main/java/be/patryksitko/contest/ip2location/com/controller/UserController.java
@@ -2,7 +2,6 @@ package be.patryksitko.contest.ip2location.com.controller;
 
 import java.util.List;
 
-import javax.security.auth.login.CredentialException;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 import be.patryksitko.contest.ip2location.com.builders.ResponseBuilder;
 import be.patryksitko.contest.ip2location.com.builders.ResponseBuilder.ResponseType;
 import be.patryksitko.contest.ip2location.com.model.User;
-import be.patryksitko.contest.ip2location.com.service.CredentialService;
 import be.patryksitko.contest.ip2location.com.service.UserService;
 import be.patryksitko.contest.ip2location.com.service.exception.EmailRegisteredException;
 import lombok.extern.slf4j.Slf4j;
@@ -30,19 +28,16 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-    @Autowired
-    private CredentialService credentialService;
 
     @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody ResponseEntity<Object> registerUser(@RequestBody @Valid User user) {
         System.out.println(user.toJSON());
         try {
-            credentialService.saveCredential(user.getCredential());
             userService.registerUser(user);
-        } catch (EmailRegisteredException | CredentialException ere) {
-            log.error(ere.getMessage());
+        } catch (EmailRegisteredException e) {
+            log.error(e.getMessage());
             return ResponseEntity.status(HttpStatus.CONFLICT)
-                    .body(ResponseBuilder.builder().status(HttpStatus.CONFLICT).errors(List.of(ere.getMessage()))
+                    .body(ResponseBuilder.builder().status(HttpStatus.CONFLICT).errors(List.of(e.getMessage()))
                             .type(ResponseType.ERROR).body(user.toJSON())
                             .build());
         }
--- java/web/src/main/java/be/patryksitko/contest/ip2location/com/service/implementation/UserServiceImplementation.java
+++ java/web/src/main/java/be/patryksitko/contest/ip2location/com/service/implementation/UserServiceImplementation.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import be.patryksitko.contest.ip2location.com.model.User;
+import be.patryksitko.contest.ip2location.com.repositoryDAO.CredentialRepository;
 import be.patryksitko.contest.ip2location.com.repositoryDAO.UserRepository;
 import be.patryksitko.contest.ip2location.com.service.UserService;
 import be.patryksitko.contest.ip2location.com.service.exception.EmailRegisteredException;
@@ -12,12 +13,15 @@ import be.patryksitko.contest.ip2location.com.service.exception.EmailRegisteredE
 public class UserServiceImplementation implements UserService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private CredentialRepository credentialRepository;
 
     @Override
     public User registerUser(User user) throws EmailRegisteredException, IllegalArgumentException {
         if (userRepository.findByEmail(user.getCredential().getEmail()) != null) {
             throw new EmailRegisteredException(user.getCredential().getEmail());
         }
+        credentialRepository.save(user.getCredential());
         return userRepository.save(user);
     }
 }
