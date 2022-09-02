db1c47cd17cb14365b9d3c2ef3ef1637c7da67ab
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2022-09-02 19:17:29 +0200

    Added an implemented login route.

--- java/web/src/main/java/be/patryksitko/contest/ip2location/com/controller/UserController.java
+++ java/web/src/main/java/be/patryksitko/contest/ip2location/com/controller/UserController.java
@@ -18,9 +18,13 @@ import be.patryksitko.contest.ip2location.com.builders.ResponseBuilder;
 import be.patryksitko.contest.ip2location.com.builders.ResponseBuilder.ResponseType;
 import be.patryksitko.contest.ip2location.com.helpers.BCryptPasswordEncoder;
 import be.patryksitko.contest.ip2location.com.helpers.exception.PasswordFormatException;
+import be.patryksitko.contest.ip2location.com.model.AuthenticationToken;
+import be.patryksitko.contest.ip2location.com.model.Credential;
 import be.patryksitko.contest.ip2location.com.model.User;
 import be.patryksitko.contest.ip2location.com.service.UserService;
 import be.patryksitko.contest.ip2location.com.service.exception.EmailRegisteredException;
+import be.patryksitko.contest.ip2location.com.service.exception.EmailUnregisteredException;
+import be.patryksitko.contest.ip2location.com.service.exception.PasswordMismatchException;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -47,5 +51,26 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(ResponseBuilder.builder().status(HttpStatus.CREATED)
                 .responseType(ResponseType.SUCCESS).body(user.toJSON()).build());
 
+    }
+
+    @PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody ResponseEntity<Object> loginUser(@RequestBody @Valid Credential credential) {
+        AuthenticationToken authenticationToken;
+        try {
+            authenticationToken = userService.authenticateUser(credential);
+        } catch (EmailUnregisteredException e) {
+            log.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(ResponseBuilder.builder().status(HttpStatus.NOT_FOUND).errors(List.of(e.getMessage()))
+                            .responseType(ResponseType.ERROR).body(credential.toJSON()).build());
+        } catch (PasswordMismatchException e) {
+            log.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body(ResponseBuilder.builder().status(HttpStatus.CONFLICT).errors(List.of(e.getMessage()))
+                            .responseType(ResponseType.ERROR).body(credential.toJSON()).build());
+        }
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .body(ResponseBuilder.builder().status(HttpStatus.FOUND).responseType(ResponseType.SUCCESS)
+                        .body(authenticationToken.toJSON()).build());
     }
 }
