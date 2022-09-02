3391c1007c063d6c1f88fcd289b87e50cf3b7650
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2022-09-02 17:41:39 +0200

    Added missing where clause.

--- java/web/src/main/java/be/patryksitko/contest/ip2location/com/repositoryDAO/AuthenticationTokenRepository.java
+++ java/web/src/main/java/be/patryksitko/contest/ip2location/com/repositoryDAO/AuthenticationTokenRepository.java
@@ -11,6 +11,6 @@ import be.patryksitko.contest.ip2location.com.model.AuthenticationToken;
 @Repository
 public interface AuthenticationTokenRepository extends CrudRepository<AuthenticationToken, Long> {
 
-    @Query("SELECT authToken FROM AuthenticationToken AS authToken INNER JOIN authToken.credential AS credential")
+    @Query("SELECT authToken FROM AuthenticationToken AS authToken INNER JOIN authToken.credential AS credential WHERE credential.email = ?1")
     List<AuthenticationToken> findByEmail(String email);
 }
--- java/web/src/main/java/be/patryksitko/contest/ip2location/com/repositoryDAO/UserRepository.java
+++ java/web/src/main/java/be/patryksitko/contest/ip2location/com/repositoryDAO/UserRepository.java
@@ -8,6 +8,6 @@ import be.patryksitko.contest.ip2location.com.model.User;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
-    @Query("SELECT user FROM User AS user INNER JOIN user.credential AS credential")
+    @Query("SELECT user FROM User AS user INNER JOIN user.credential AS credential WHERE credential.email = ?1")
     User findByEmail(String email);
 }
