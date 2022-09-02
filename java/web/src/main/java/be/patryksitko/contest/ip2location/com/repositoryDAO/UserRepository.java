beaffdbc1a38eb348bfaf331b9fbf94d2d9f52f6
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2022-09-02 17:26:30 +0200

    Reformatted findByMail query for user repository.

--- java/web/src/main/java/be/patryksitko/contest/ip2location/com/repositoryDAO/UserRepository.java
+++ java/web/src/main/java/be/patryksitko/contest/ip2location/com/repositoryDAO/UserRepository.java
@@ -8,6 +8,6 @@ import be.patryksitko.contest.ip2location.com.model.User;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
-    @Query("SELECT u FROM User AS u INNER JOIN u.credential AS c")
+    @Query("SELECT user FROM User AS user INNER JOIN user.credential AS credential")
     User findByEmail(String email);
 }
