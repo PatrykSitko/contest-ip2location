24a6b4d1dff13030cab0b17658768c8a0cf7458a
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2022-09-02 19:16:34 +0200

    Added password mismatch exception to throws clause of authenticate user.

--- java/web/src/main/java/be/patryksitko/contest/ip2location/com/service/UserService.java
+++ java/web/src/main/java/be/patryksitko/contest/ip2location/com/service/UserService.java
@@ -1,12 +1,11 @@
 package be.patryksitko.contest.ip2location.com.service;
 
-import java.util.Optional;
-
 import be.patryksitko.contest.ip2location.com.model.AuthenticationToken;
 import be.patryksitko.contest.ip2location.com.model.Credential;
 import be.patryksitko.contest.ip2location.com.model.User;
 import be.patryksitko.contest.ip2location.com.service.exception.EmailRegisteredException;
 import be.patryksitko.contest.ip2location.com.service.exception.EmailUnregisteredException;
+import be.patryksitko.contest.ip2location.com.service.exception.PasswordMismatchException;
 
 public interface UserService {
 
@@ -14,5 +13,6 @@ public interface UserService {
 
     public User findUserByEmail(String email) throws EmailUnregisteredException;
 
-    public Optional<AuthenticationToken> authenticateUser(Credential credential) throws EmailUnregisteredException;
+    public AuthenticationToken authenticateUser(Credential credential)
+            throws EmailUnregisteredException, PasswordMismatchException;
 }
