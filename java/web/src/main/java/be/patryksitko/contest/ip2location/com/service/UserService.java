d822532d612e4fcafa31db59360163b76fc0c06a
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2022-09-02 17:27:33 +0200

    Added authenticateUser method reference in the UserService interface.

--- java/web/src/main/java/be/patryksitko/contest/ip2location/com/service/UserService.java
+++ java/web/src/main/java/be/patryksitko/contest/ip2location/com/service/UserService.java
@@ -1,5 +1,9 @@
 package be.patryksitko.contest.ip2location.com.service;
 
+import java.util.Optional;
+
+import be.patryksitko.contest.ip2location.com.model.AuthenticationToken;
+import be.patryksitko.contest.ip2location.com.model.Credential;
 import be.patryksitko.contest.ip2location.com.model.User;
 import be.patryksitko.contest.ip2location.com.service.exception.EmailRegisteredException;
 import be.patryksitko.contest.ip2location.com.service.exception.EmailUnregisteredException;
@@ -9,4 +13,6 @@ public interface UserService {
     public User registerUser(User user) throws EmailRegisteredException;
 
     public User findUserByEmail(String email) throws EmailUnregisteredException;
+
+    public Optional<AuthenticationToken> authenticateUser(Credential credential) throws EmailUnregisteredException;
 }
