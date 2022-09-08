4e43034f2f0691ba0337c2c2a925257d640ccc1a
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2022-09-08 13:05:37 +0200

    Changed from post to put mapping for register route.

--- java/web/src/main/java/be/patryksitko/contest/ip2location/com/controller/UserController.java
+++ java/web/src/main/java/be/patryksitko/contest/ip2location/com/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -35,7 +36,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody ResponseEntity<Object> registerUser(@RequestBody @Valid User user) {
         try {
             if (BCryptPasswordEncoder.isPasswordMeetingStandards(user.getCredential().getPassword())) {
