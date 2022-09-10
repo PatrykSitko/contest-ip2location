8ccc3db87cbc93330d698cdaab074d784d2a903a
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2022-09-10 10:50:07 +0200

    Finnished the hook (for now).

--- java/web/src/main/javascript/be/patryksitko/contest/ip2location/com/frontend/src/hooks/useCheckAuthenticationToken.js
+++ java/web/src/main/javascript/be/patryksitko/contest/ip2location/com/frontend/src/hooks/useCheckAuthenticationToken.js
@@ -1,7 +1,43 @@
+import httpStatus from "http-status";
+import Cookies from "js-cookie";
 import { useEffect } from "react";
 
-function useCheckAuthenticationToken(fingerprint) {
-  useEffect(() => {}, []);
+function useCheckAuthenticationToken(
+  csrfToken,
+  fingerprint,
+  authenticationToken
+) {
+  useEffect(() => {
+    (async () => {
+      if (!fingerprint) {
+        return;
+      }
+      const result = await fetch("/api/user/validate-authentication-token", {
+        headers: {
+          "CSRF-TOKEN": csrfToken,
+          "Content-Type": "Application/json",
+        },
+        method: "POST",
+        body: JSON.stringify({
+          authenticationToken,
+          fingerprint: fingerprint.visitorId,
+        }),
+      });
+      const { status, body: body_ } = await result.json();
+      const { isProvidedAuthenticationTokenValid } = JSON.parse(body_);
+      switch (httpStatus[status]) {
+        default:
+        case httpStatus.ACCEPTED:
+          break;
+        case httpStatus.UNPROCESSABLE_ENTITY:
+        case httpStatus.NOT_ACCEPTABLE:
+          if (!isProvidedAuthenticationTokenValid) {
+            Cookies.remove("authentication-token");
+          }
+          break;
+      }
+    })();
+  }, [csrfToken, fingerprint, authenticationToken]);
 }
 
 export default useCheckAuthenticationToken;
